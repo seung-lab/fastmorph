b4acbde8507d6c148b4d0e895789492630ce53a9
William Silversmith <william.silversmith@gmail.com>
2022-09-05 16:10:46 -0400

    feat: add in_place argument

--- fastmorph/__init__.py
+++ fastmorph/__init__.py
@@ -11,47 +11,83 @@ def spherical_dilate(
   labels:np.ndarray, 
   radius:float = 1.0, 
   parallel:int = 1, 
-  anisotropy:AnisotropyType = None
+  anisotropy:AnisotropyType = None,
+  in_place:bool = False,
 ) -> np.ndarray:
   """
   Dilate foreground binary components.
+
+  Uses the exact euclidean distance transform to determine voxel distances
+  (hence spherical). Does not use a structuring element.
+
+  labels: input labels (must be a boolean image)
+  radius: physical distance (considering anisotropy) to dilate to (inclusive range)
+  parallel: use this many threads to compute the distance transform
+  anisotropy: voxel resolution in x, y, and z
+  in_place: save memory by modifying labels directly instead of creating a new image
+
+  Returns: dilated binary iamge
   """
   assert np.issubdtype(labels.dtype, bool), "Dilation is currently only supported for binary images."
   dt = edt.edt(labels == 0, parallel=parallel, anisotropy=anisotropy)
+  
+  if in_place:
+    labels |= (dt <= radius)
+    return labels
+
   return labels | (dt <= radius)
 
 def spherical_erode(
   labels:np.ndarray, 
   radius:float = 1.0, 
   parallel:int = 1, 
-  anisotropy:AnisotropyType = None
+  anisotropy:AnisotropyType = None,
+  in_place:bool = False,
 ) -> np.ndarray:
   """
-  Erode foreground binary components.
+  Erode foreground multi-label components.
+
+  Uses the exact euclidean distance transform to determine voxel distances
+  (hence spherical). Does not use a structuring element.
+
+  labels: input labels (must be a boolean image)
+  radius: physical distance (considering anisotropy) to dilate to (inclusive range)
+  parallel: use this many threads to compute the distance transform
+  anisotropy: voxel resolution in x, y, and z
+  in_place: save memory by modifying labels directly instead of creating a new image
+
+  Returns: eroded multi-label iamge  
   """
   dt = edt.edt(labels, parallel=parallel, anisotropy=anisotropy, black_border=True)
-  return (labels * (dt >= radius)).astype(labels.dtype, copy=False)
+
+  binary_image = dt >= radius
+  if in_place:
+    labels *= binary_image
+    return labels
+  return (labels * binary_image).astype(labels.dtype, copy=False)
 
 def spherical_open(
   labels:np.ndarray, 
   radius:float = 1.0,
   parallel:int = 1, 
-  anisotropy:AnisotropyType = None
+  anisotropy:AnisotropyType = None,
+  in_place:bool = False,
 ) -> np.ndarray:
   """Apply a spherical morphological open operation to a binary image."""
   assert np.issubdtype(labels.dtype, bool), "spherical_open is currently only supported for binary images."
-  args = [ radius, parallel, anisotropy ]
+  args = [ radius, parallel, anisotropy, in_place ]
   return spherical_dilate(spherical_erode(labels, *args), *args)
 
 def spherical_close(
   labels:np.ndarray, 
   radius:float = 1.0,
   parallel:int = 1, 
-  anisotropy:AnisotropyType = None
+  anisotropy:AnisotropyType = None,
+  in_place:bool = False,
 ) -> np.ndarray:
   """Apply a spherical morphological close operation to a binary image."""
   assert np.issubdtype(labels.dtype, bool), "spherical_close is currently only supported for binary images."
-  args = [ radius, parallel, anisotropy ]
+  args = [ radius, parallel, anisotropy, in_place ]
   return spherical_erode(spherical_dilate(labels, *args), *args)
 
 
