f2f0d5d98b00b279c484f248dcc1348259302185
William Silversmith <william.silversmith@gmail.com>
2025-02-20 22:53:18 -0500

    release(1.3.1): license change to LGPLv3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setuptools.setup(
   name="fastmorph",
-  version="1.3.0",
+  version="1.3.1",
   setup_requires=["numpy","pybind11"],
   install_requires=['numpy', 'edt', 'fill-voids', 'connected-components-3d', 'fastremap'],
   python_requires=">=3.8.0", # >= 3.8 < 4.0
